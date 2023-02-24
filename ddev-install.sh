f756cc37f8d8662f259ab7e0fc6e59177be40893
Jair Santana <feodpv03262@odontoprev.com.br>
2023-02-24 17:38:12 -0300

    script install ddev

---
+++ ddev-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -fsSL https://apt.fury.io/drud/gpg.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ddev.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/ddev.gpg] https://apt.fury.io/drud/ * *" | sudo tee /etc/apt/sources.list.d/ddev.list
+sudo apt update && sudo apt install -y ddev
